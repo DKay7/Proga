d6da5bc782548199da2f43242b8e501d780af7e5
Daniil.K <dlkay@yandex.ru>
2021-03-23 12:18:49 +0300

    Add 1b task

--- c/c-tasks/1b.c
+++ c/c-tasks/1b.c
@@ -6,17 +6,19 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
+Node *head = NULL;
+
 
 int push(Node**, int);
 int pop(Node**);
-int print(Node**);
-int input(Node**);
+void print(Node*);
+void input(Node**);
 
 int main() 
 {
 }
 
-int push(Node** head, int data)
+int push(Node **head, int data)
 {
     Node *tmp = (Node*)malloc(sizeof(Node));
 
@@ -48,5 +50,95 @@ int pop(Node **head)
     return value;
 }
 
-int print(Node**);
-int input(Node**);
+int getLastVal(Node **head) {
+
+    int value = 0;
+
+    if (head == NULL) {
+        return -1;
+    }
+
+    value = (*head)->value;
+    return value;
+}
+
+
+void print(Node *head)
+{
+    while (head) 
+    {
+        printf("%d ", head->value);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+
+void input(Node **head)
+{   
+    int c = 0, i=0;
+
+    while((c = getchar()) != EOF)
+    {
+        push(head, c);
+    }
+}
+
+void countOddPos(Node **head, int num_pos)
+{
+    int i = 0, val = 0, count = 0;
+
+    for (i; i < num_pos, head != NULL; i++, head = (*head)->next)
+    {
+        val = get_last_val(head);
+
+        if (val == 0)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+int countSize(Node **head)
+{   
+    int size = 0;
+
+    while(head)
+    {
+        (*head) = (*head)->next;
+        size += 1;
+    }
+
+    return size;
+}
+
+void changeEven(Node *head1, Node *head2, int num_pos)
+{
+    int i = 0, val1 = 0, val2 = 0, tmp = 0;
+
+    int size = countSize(&head1);
+    for (i; i < size - num_pos; i++)
+    {
+
+    }
+    
+    for (i = num_pos; i >= size; i--)
+    {   
+        if (i % 2 == 0)
+        {
+            val1 = head1->value;
+            val2 = head2->value;
+
+            head1->value = val2;
+            head2->value = val1;
+        }
+
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    return count;
+}
